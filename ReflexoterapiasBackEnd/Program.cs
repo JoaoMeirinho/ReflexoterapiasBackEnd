b54956224e7baef4353d9dfbfe4a42e8d93ff707
João Meirinho <meirinhojoao2004@gmail.com>
2025-07-26 21:37:39 -0300

    apply clean arch

--- ReflexoterapiasBackEnd/Program.cs
+++ ReflexoterapiasBackEnd/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using ReflexoterapiasBackEnd.DBConfig;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseMySql(
+        connectionString,
+        ServerVersion.AutoDetect(connectionString)
+    )
+);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
